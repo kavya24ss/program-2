fa757e6460e939c6a89b61c26e2c4f409964c744
kavya24ss <sagikavyasri@gmail.com>
2025-10-17 21:47:09 +0530

    Update program-2.py

--- program-2.py
+++ program-2.py
@@ -1,6 +1,11 @@
+
 salary = float(input("Enter your salary: "))
 bonus = salary * 0.10
-total_salary = salary + bonus
+total = salary + bonus
+tax = total * 0.05
+final_salary = total - tax
 
 print("Original Salary:", salary)
-print("Bonus Added:", total_salary)
+print("Bonus Added:", total)
+print("After Tax Deduction:", final_salary)
+
